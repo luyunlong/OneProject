03d09229e8a005c2e99e368d92a625bd3f922e7d
Stone <514122102@qq.com>
2015-10-17 14:38:39 +0800

    创建项目

---
+++ oneproject/oneproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oneproject
+//
+//  Created by 卢麒麟 on 15/10/17.
+//  Copyright © 2015年 luyunlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ oneproject/oneprojectTests/oneprojectTests.m
@@ -0,0 +1,39 @@
+//
+//  oneprojectTests.m
+//  oneprojectTests
+//
+//  Created by 卢麒麟 on 15/10/17.
+//  Copyright © 2015年 luyunlong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface oneprojectTests : XCTestCase
+
+@end
+
+@implementation oneprojectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
